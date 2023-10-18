7d2fae0ded9166f738693b131215168a8165283d
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-18 20:39:08 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/sagachoreographypattern1/SagaChoreographyPattern1Application.java
@@ -0,0 +1,13 @@
+package com.hemanth.sagachoreographypattern1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaChoreographyPattern1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaChoreographyPattern1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hemanth/sagachoreographypattern1/SagaChoreographyPattern1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.sagachoreographypattern1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaChoreographyPattern1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
